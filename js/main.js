5a230fc3c296eca83b2d9c2c2b62aea09523059d
nakrekarpay1245 <rknyprk2001@gmail.com>
2022-01-02 11:04:31 +0300

    2.01.2022
    first commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+function HypocrisyIsNotReady() {
+    alert("Üzgünüm :( Bu proje henüz hazır değil")
+}
+
+function AddressOpen(adres) {
+    window.open(adres, "_blank");
+}
+
+function OpenYoutube() {
+    AddressOpen("https://www.youtube.com/channel/UC4jHXpzFVgIpLGCJog9RUQw");
+}
+
+function OpenMedium() {
+    AddressOpen("https://rknyprkblog.medium.com");
+}
+
+function OpenGitHub() {
+    AddressOpen("https://github.com/nakrekarpay1245");
+}
+
+function OpenUnity() {
+    AddressOpen("https://unity.com");
+}
+
+function OpenUnreal() {
+    AddressOpen("https://www.unrealengine.com/en-");
+}
